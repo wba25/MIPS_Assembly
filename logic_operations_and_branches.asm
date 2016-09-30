0b2e2e5141f8d5a129d522d388f2034e4e32c10b
Wellington Barbosa <wba@cin.ufpe.br>
2016-09-29 23:27:23 -0300

    Testando Braches e operadores lógicos

--- logic_operations_and_branches.asm
+++ logic_operations_and_branches.asm
@@ -6,8 +6,36 @@
 	lw $t0, a($zero)
 	lw $t1, b($zero)
 	
+	#-------------------------------------------------
 	#Operaçoes logicas
-	#Tipo R
+	
+	#-Tipo R
 	#AND - and bit a bit entre 2 registradores(de 32 bits, eh claro), o resultado eh salvo no registrador do campo rt
 	and $t2, $t0, $t1 
+	#OR - eh um or
+	or $t2, $t0, $t1
+	#XOR - eh um xor e nada mais,hahaha
+	xor $t2, $t0, $t1
+	#SLT - Se $t0 for menor que $t1, $t2 recebe 1, caso contrario,recebe 0
+	slt $t2, $t0, $t1
+	#O slt muitas vezes eh usado em conjunto com algum branch (beq, bne, ...)
+	
+	#-Tipo I
+	#versoes das anteriores com imediato
+	andi $t2, $t0, 1000
+	ori $t2, $t0, 10
+	xor $t2, $t0, 10000
+	slti $t2, $t0, 7
+	
+	#-------------------------------------------------
+	#Branches
+	#-Tipo I - Todo Branch eh do tipo I
+	#BEQ - Branch - bifurca o fluxo de dados: para um caminho se a condiçao for aceita para outro caso nao	
+	#    - Para beq a condiçao eh: se for igual
+	beq $t1, $t0, LABEL1;
+	#Branch se forem diferentes
+	bne $t1, $t0, LABEL1;	
+	
+	LABEL1: #nothing
+	
 	
